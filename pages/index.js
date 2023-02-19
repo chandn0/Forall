630ef719ef10a71f8d72ffd6720d418ca4aeb86a
chandn0 <boinapallychandan007@gmail.com>
2023-02-19 11:30:23 +0530

    ui

--- pages/index.js
+++ pages/index.js
@@ -19,19 +19,17 @@ export default function Home() {
         <div className={styles.hero_img}>
           {/* <Image src={landing} /> */}
         </div>
-        <h3 className={styles.heading}>
-          Did you find someone misusing their power, but you do not have the courage to speak up?
-          <br />
-          <br />
-          Submit the proof and let others take charge.
+        <h3 className={styles.mainheading}>
+          If you have evidence of someone misusing their power but feel hesitant to speak up due to concerns about potential repercussions, our platform provides a safe and anonymous way to report the misconduct. By submitting your proof, you can empower others to take action and hold the individual accountable
         </h3>
       </main>
       <div className={styles.features}>
         {/* <h2 className={styles.heading}>Key Features</h2> */}
-        <ul className={styles.feature}>
-          <li>You real world identity can not  be traced</li>
-          <li>Proofs are stored in form of NFT allowing you to trade</li>
-          <li>All the proofs are stored on-chain can not be Destroyed</li>
+        <ul className={styles.mainfeature}>
+          <li>To ensure your anonymity, we do not require you to reveal your real-world identity when submitting your evidence. We take this protection very seriously and use a variety of security measures to safeguard your identity.</li>
+          <li>In addition, all proofs submitted to our platform are stored in the form of NFTs (Non-Fungible Tokens), a type of digital asset that provides unique ownership and authenticity to the content. This means that you can retain ownership of your proof and even trade it if you wish to do so.</li>
+          <li>Moreover, all the proofs submitted to our platform are stored on-chain, meaning that they are recorded on a blockchain, a decentralized and tamper-proof digital ledger. This ensures that your proof cannot be destroyed, altered, or lost.</li>
+          <li>With our platform, you can take a stand against misconduct and make a difference while protecting your anonymity and your evidence.</li>
         </ul>
       </div>
       <div className={styles.features}>
