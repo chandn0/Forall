cc3621fecdd191cb3a7919a8dca4d0c4cee97246
chandn0 <boinapallychandan007@gmail.com>
2023-02-19 12:07:04 +0530

    ui

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import web3storage from "../src/assets/web3storage.svg";
 import polygon from "../src/assets/polygon.svg";
 import ipfs from "../src/assets/ipfs.png";
 import styles from "../styles/Home.module.css";
+import { stay_anynon } from "../src/assets/stay_anynon.png";
 
 export default function Home() {
   return (
@@ -26,7 +27,7 @@ export default function Home() {
       <div className={styles.features}>
         {/* <h2 className={styles.heading}>Key Features</h2> */}
         <ul className={styles.mainfeature}>
-          <li>To ensure your anonymity, we do not require you to reveal your real-world identity when submitting your evidence. We take this protection very seriously and use a variety of security measures to safeguard your identity.</li>
+          <li>  To ensure your anonymity, we do not require you to reveal your real-world identity when submitting your evidence. We take this protection very seriously and use a variety of security measures to safeguard your identity.</li>
           <li>In addition, all proofs submitted to our platform are stored in the form of NFTs (Non-Fungible Tokens), a type of digital asset that provides unique ownership and authenticity to the content. This means that you can retain ownership of your proof and even trade it if you wish to do so.</li>
           <li>Moreover, all the proofs submitted to our platform are stored on-chain, meaning that they are recorded on a blockchain, a decentralized and tamper-proof digital ledger. This ensures that your proof cannot be destroyed, altered, or lost.</li>
           <li>With our platform, you can take a stand against misconduct and make a difference while protecting your anonymity and your evidence.</li>
@@ -39,11 +40,11 @@ export default function Home() {
           <li>Move to submit page </li>
           <li>Add Title,Description and create an image and add the CID links </li>
           <li>Click on mint </li>
-          <li>The contract Address: <a
+          <li >The contract Address: <a
             target="_blank"
-            href={"https://etherscan.io/address/0x5f64a2f3c1b0e7b6e6b7b3d5a6f5afef099e4b36"}
+            href={"https://mumbai.polygonscan.com/address/0x0b36cacc1232981ef95bde16191bda7d0216d2a8"}
             rel="noopener noreferrer"
-          >Etherscan</a></li>
+          >Mumbai_Scan</a></li>
         </ul>
 
       </div>
